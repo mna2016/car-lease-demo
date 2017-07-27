d8e2fad5adeb105769ef76ee0b3b1b33e67eedc6
mna2016 <nihal.ansari@in.ibm.com>
2017-07-28 00:58:17 +0530

    Compile error
    fix4

--- Chaincode/src/vehicle_code/vehicles.go
+++ Chaincode/src/vehicle_code/vehicles.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"bytes"
 //	"strconv"
 	"strings"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
@@ -988,7 +989,7 @@ func (t *SimpleChaincode) get_vehicle_details(stub shim.ChaincodeStubInterface, 
 	
 	//bytes2 :=  msgpart1 + bytes + msgpart2	
 	
-	bytes2:=[]byte{msgpart1,bytes,msgpart2};
+	bytes2:=bytes.Join(msgpart1,bytes,msgpart2);
 	
 	if err != nil { return nil, errors.New("READASSET: Invalid vehicle object") }
 
