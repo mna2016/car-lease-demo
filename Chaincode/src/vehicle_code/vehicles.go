cab43726b984baa65508f3f232b6cd9324abb455
mna2016 <nihal.ansari@in.ibm.com>
2017-07-28 00:51:53 +0530

    Compile error
    fix3

--- Chaincode/src/vehicle_code/vehicles.go
+++ Chaincode/src/vehicle_code/vehicles.go
@@ -986,7 +986,10 @@ func (t *SimpleChaincode) get_vehicle_details(stub shim.ChaincodeStubInterface, 
 
 	//bytes2 := append(msgpart1,bytes,msgpart2)	
 	
-	bytes2 :=  msgpart1 + bytes + msgpart2	
+	//bytes2 :=  msgpart1 + bytes + msgpart2	
+	
+	bytes2:=[]byte{msgpart1,bytes,msgpart2};
+	
 	if err != nil { return nil, errors.New("READASSET: Invalid vehicle object") }
 
 	if 		v.OwnerId	== caller		||
