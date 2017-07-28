64003e33d5c70211e0c4bb9e1a49ec2861d3cf91
mna2016 <nihal.ansari@in.ibm.com>
2017-07-28 17:38:15 +0530

    compile error
    fixed

--- Chaincode/src/vehicle_code/vehicles.go
+++ Chaincode/src/vehicle_code/vehicles.go
@@ -330,7 +330,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	//Now assign the real arguments to object animal 
 	animals = inreq.Asset.Anim
 	
-	return nil, errors.New("INPUT REQUEST:" + Args[1]+ "animals struct :"+ animals.AssetID + ":" + animals.Caller + ":" )
+	return nil, errors.New("INPUT REQUEST:" + Args[1]+ "animals struct :"+ animals.AssetId + ":" + animals.Caller + ":" )
 	// IMPORTANT: v5cid variable is used in most of the places in this contract
 	// the frontend will pass the field assetID as the
 	// copy assetID over to v5cid here
