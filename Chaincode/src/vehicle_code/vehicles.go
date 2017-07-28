baa11368f851b9a5034c2f942fe572885ad13022
mna2016 <nihal.ansari@in.ibm.com>
2017-07-28 17:33:36 +0530

    reformat input request
    debug msg temp change

--- Chaincode/src/vehicle_code/vehicles.go
+++ Chaincode/src/vehicle_code/vehicles.go
@@ -328,8 +328,9 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	}
     
 	//Now assign the real arguments to object animal 
-	&animals = &inreq.Asset.Anim
+	animals = inreq.Asset.Anim
 	
+	return nil, errors.New("INPUT REQUEST:" + Args[1]+ "animals struct :"+ animals.assetID + ":" + animals.caller + ":" )
 	// IMPORTANT: v5cid variable is used in most of the places in this contract
 	// the frontend will pass the field assetID as the
 	// copy assetID over to v5cid here
